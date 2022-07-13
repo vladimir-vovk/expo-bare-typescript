c8888b93dd8a744bbb922b1f1442d22080afcce5
Vladimir Vovk <vladimir@vovk.in>
2022-07-13 22:24:07 +0300

    feat(imports): add sort imports on save

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,4 +5,14 @@ module.exports = {
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  importOrderCaseInsensitive: true,
+  importOrder: [
+    '<THIRD_PARTY_MODULES>',
+    // '^(.*)/components/(.*)$', // Add any folders you want to be separate
+    '^(.*)/(?!generated)(.*)/(.*)$', // Everything not generated
+    '^(.*)/generated/(.*)$', // Everything generated
+    '^[./]' // Absolute path imports
+  ]
 }
